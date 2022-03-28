56daf28d729049d25621cdadf38f9b235066024a
Kinga Kohari <kinguca@yahoo.com>
2022-03-28 19:29:31 +0200

    Image upload works

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,65 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const fs = require("fs");
 const app = express(); // express is egy függvény és ez lefut, és objektum jön vissza
 
-// default options
-app.use(fileUpload());
-
 const path = require("path");
+const dataLocation = path.join(`${__dirname}/../frontend/data/`);
 
-function getFunction(request, response){
-    response.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+function getFunction(req, res){
+    res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 }
 
 app.use(fileUpload());
-
-app.get("/", getFunction);
 app.use("/upload", express.static(`${__dirname}/../frontend/upload`));
 app.use("/pub", express.static(`${__dirname}/../frontend/public`));
 
+app.get("/", getFunction);
+
 const uploads = path.join(`${__dirname}/../frontend/upload/`)
 
+// If there is a data.json, read the data from the file, if not, use an empty Array
+let jsonData = [];
+try {
+    let data = fs.readFileSync(`${dataLocation}data.json`, error => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    jsonData = JSON.parse(data);
+} catch (error) {
+    fs.writeFile(`${dataLocation}data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
+
 app.post("/", (req, res) => {
-	//Upload image
-	const image = req.files.picture
-	const answer = {}
+    // Upload image
+    const picture = req.files.picture;
+    const answer = {};
 
-	if(image) {
-		console.dir(image)
-		
-		image.mv(uploads + image.name)
-	}
+    if (picture) {
+        picture.mv(uploads + picture.name, error => {
+            return res.status(500).send(error);
+        });
+    }
+    answer.pictureName = picture.name;
 
-	answer.imageName = image.name
-	res.send(answer)
+    // Upload data from form
+    const formData = req.body;
+    formData.image_name = picture.name;
+    jsonData.push(formData);
 
-
-	//Upload data from form
-	const formData = req.body
-	formData.image_name = picture.name
-	jsonData.push(formData)
-
+    fs.writeFile(`${dataLocation}data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    res.send(answer);
 });
+
 
 const port = 9000;
 const ipAddress = "http://127.0.0.1:9000";
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -10,33 +10,33 @@ const formComponent = `
 function loadEvent() {
     console.log("hello")
 
-    const rootElement = document.getElementById('root')
+    const rootElement = document.getElementById("root")
     rootElement.insertAdjacentHTML("beforeend", formComponent);
 
     const formElement = document.getElementById('form')
-    formElement.addEventListener("submit", (e) =>{
+    formElement.addEventListener("submit", (e) => {
         e.preventDefault();
-        console.dir('e');
+        console.dir(e);
 
         const formData = new FormData();
-        formData.append("title", e.target.querySelector(input[name="title"]).value);
-        formData.append("picture", e.target.querySelector(input[name="picture"]).files[0]);
+        formData.append("title", e.target.querySelector(`input[name="title"]`).value);
+        formData.append("picture", e.target.querySelector(`input[name="picture"]`).files[0]);
 
         const fetchSettings = {
             method: "POST",
             body: formData
         }
 
-        fetch('/', fetchSettings)
+        fetch("/", fetchSettings)
             .then(async data => {
                 if (data.status === 200){
                     const res = await data.json()
-                    e.target.outerHTML = `<img src="${res.imageName}">`;
+                    e.target.outerHTML = `<img src="upload/${res.pictureName}">`;
                     console.dir(data);
                 }
             })
             .catch(error => {
-                e.target.outerHTML= '${error}'
+                e.target.outerHTML= `Error`
                 console.dir(error);
             })
     });
